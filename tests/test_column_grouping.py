ee94334d50412adff19c7bcbe865e4a09175abd0
Prosto Data <52108119+prostodata@users.noreply.github.com>
2019-07-21 17:48:24 +0200

    add tests for link and grouping operations

---
+++ tests/test_column_grouping.py
@@ -0,0 +1,79 @@
+import unittest
+
+from prosto.Schema import *
+
+class ColumnGroupingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_aggregate(self):
+        sch = Schema("My schema")
+
+        # Facts
+        f_tbl = sch.create_populate_table(
+            table_name="Facts", attributes=["A", "M"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'a', 'b', 'b'], 'M': [1.0, 2.0, 3.0, 4.0]})", tables=[], model=None, input_length='table'
+        )
+
+        # Groups
+        df = pd.DataFrame({'A': ['a', 'b', 'c']})
+        g_tbl = sch.create_populate_table(
+            table_name="Groups", attributes=["A"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'b', 'c']})", tables=[], model=None, input_length='table'
+        )
+
+        # Link
+        l_clm = sch.create_link_column(
+            name="Link", table=f_tbl.id, type=g_tbl.id,
+            columns=["A"], linked_columns=["A"]
+        )
+
+        # Aggregation
+        a_clm = sch.create_grouping_column(
+            name="Aggregate", table=g_tbl.id,
+            tables=["Facts"], link="Link",
+            func="lambda x, bias,**model: x.sum() + bias", columns=["M"], model={"bias": 0.0}, input_length='column'
+        )
+
+        f_tbl.evaluate()
+        g_tbl.evaluate()
+
+        l_clm.evaluate()
+        a_clm.evaluate()
+
+        g_tbl_data = g_tbl.get_data()
+        self.assertEqual(len(g_tbl_data), 3)  # Same number of rows
+        self.assertEqual(len(g_tbl_data.columns), 2)  # One aggregate column was added (and one technical "id" column was added which might be removed in future)
+
+        a_clm_data = g_tbl.get_column_data('Aggregate')
+        self.assertEqual(a_clm_data[0], 3.0)
+        self.assertEqual(a_clm_data[1], 7.0)
+        self.assertEqual(a_clm_data[2], 0.0)
+
+        #
+        # Test topology
+        #
+        topology = Topology(sch)
+        topology.translate()
+        layers = topology.elem_layers
+
+        self.assertEqual(len(layers), 3)
+
+        self.assertTrue(set([x.id for x in layers[0]]) == set(["Facts", "Groups"]))
+        self.assertTrue(set([x.id for x in layers[1]]) == set(["Link"]))
+        self.assertTrue(set([x.id for x in layers[2]]) == set(["Aggregate"]))
+
+        a_clm_data = g_tbl.get_column_data('Aggregate')
+        a_clm_data.values.fill(0.0)
+
+        sch.run()
+
+        a_clm_data = g_tbl.get_column_data('Aggregate')
+        self.assertEqual(a_clm_data[0], 3.0)
+        self.assertEqual(a_clm_data[1], 7.0)
+        self.assertEqual(a_clm_data[2], 0.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_column_link.py
@@ -0,0 +1,119 @@
+import unittest
+
+from prosto.Schema import *
+
+class ColumnLinkTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_one_key(self):
+        sch = Schema("My schema")
+
+        # Facts
+        f_tbl = sch.create_populate_table(
+            table_name="Facts", attributes=["A"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'a', 'b', 'b']})", tables=[], model=None, input_length='table'
+        )
+
+        # Groups
+        g_tbl = sch.create_populate_table(
+            table_name="Groups", attributes=["A"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'b', 'c']})", tables=[], model=None, input_length='table'
+        )
+
+        # Link
+        l_clm = sch.create_link_column(
+            name="Link", table=f_tbl.id, type=g_tbl.id,
+            columns=["A"], linked_columns=["A"]
+        )
+
+        f_tbl.evaluate()
+        g_tbl.evaluate()
+
+        l_clm.evaluate()
+
+        f_tbl_data = f_tbl.get_data()
+        self.assertEqual(len(f_tbl_data), 4)  # Same number of rows
+        self.assertEqual(len(f_tbl_data.columns), 2)
+
+        l_data = f_tbl.get_column_data("Link")
+        self.assertEqual(l_data[0], 0)
+        self.assertEqual(l_data[1], 0)
+        self.assertEqual(l_data[2], 1)
+        self.assertEqual(l_data[3], 1)
+
+        #
+        # Test topology
+        #
+        topology = Topology(sch)
+        topology.translate()
+        layers = topology.elem_layers
+
+        self.assertEqual(len(layers), 2)
+
+        self.assertTrue(set([x.id for x in layers[0]]) == set(["Facts", "Groups"]))
+        self.assertTrue(set([x.id for x in layers[1]]) == set(["Link"]))
+
+    def test_two_keys(self):
+        sch = Schema("My schema")
+
+        # Facts
+        f_tbl = sch.create_populate_table(
+            table_name="Facts", attributes=["A", "B"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'b', 'b', 'a'], 'B': ['b', 'c', 'c', 'a']})", tables=[], model=None, input_length='table'
+        )
+
+        # Groups
+        g_tbl = sch.create_populate_table(
+            table_name="Groups", attributes=["A", "B"],
+            func="lambda **m: pd.DataFrame({'A': ['a', 'b', 'a'], 'B': ['b', 'c', 'c'], 'C': [1, 2, 3]})", tables=[], model=None, input_length='table'
+        )
+
+        # Link
+        l_clm = sch.create_link_column(
+            name="Link", table=f_tbl.id, type=g_tbl.id,
+            columns=["A", "B"], linked_columns=["A", "B"]
+        )
+
+        f_tbl.evaluate()
+        g_tbl.evaluate()
+
+        l_clm.evaluate()
+
+        f_tbl_data = f_tbl.get_data()
+        self.assertEqual(len(f_tbl.data), 4)  # Same number of rows
+        self.assertEqual(len(f_tbl.data.columns), 3)
+
+        l_data = f_tbl.get_column_data("Link")
+        self.assertEqual(l_data[0], 0)
+        self.assertEqual(l_data[1], 1)
+        self.assertEqual(l_data[2], 1)
+        self.assertTrue(pd.isna(l_data[3]))
+
+        #
+        # Test topology
+        #
+        topology = Topology(sch)
+        topology.translate()
+        layers = topology.elem_layers
+
+        self.assertEqual(len(layers), 2)
+
+        self.assertTrue(set([x.id for x in layers[0]]) == set(["Facts", "Groups"]))
+        self.assertTrue(set([x.id for x in layers[1]]) == set(["Link"]))
+
+        l_data = f_tbl.get_column_data("Link")
+        l_data.values.fill(0)
+
+        sch.run()
+
+        l_data = f_tbl.get_column_data("Link")
+        self.assertEqual(l_data[0], 0)
+        self.assertEqual(l_data[1], 1)
+        self.assertEqual(l_data[2], 1)
+        self.assertTrue(pd.isna(l_data[3]))
+
+
+if __name__ == '__main__':
+    unittest.main()
