1c487410d1eed757ac439a1b29407804763f10c0
Alexandr Savinov <savinov@conceptoriented.org>
2019-07-22 20:50:24 +0200

    add tests for product and filter operations

---
+++ tests/test_table_filter.py
@@ -0,0 +1,53 @@
+import unittest
+
+from prosto.Schema import *
+
+class TableFilterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_filter_table(self):
+        sch = Schema("My schema")
+
+        tbl = sch.create_populate_table(
+            table_name="Base table", attributes=["A", "B"],
+            func="lambda **m: pd.DataFrame({'A': [1.0, 2.0, 3.0], 'B': ['x', 'yy', 'zzz']})", tables=[], model=None, input_length='table'
+        )
+
+        clm = sch.create_calculate_column(
+            name="filter_column", table=tbl.id,
+            func="lambda x, param: (x['A'] > param) & (x['B'].str.len() < 3)",  # Return a boolean Series
+            columns=["A", "B"], model={"param": 1.5}, input_length='column'
+        )
+
+        tbl.evaluate()
+        clm.evaluate()
+
+        tbl = sch.create_filter_table(
+            table_name="Filtered table", attributes=["super"],
+            func=None, tables=["Base table"], columns=["filter_column"]
+        )
+
+        tbl.evaluate()
+
+        self.assertEqual(len(tbl.data.columns), 1)  # Only one link-attribute is created
+        self.assertEqual(len(tbl.data), 1)
+        self.assertEqual(tbl.data['super'][0], 1)
+
+        #
+        # Test topology
+        #
+        topology = Topology(sch)
+        topology.translate()
+        layers = topology.elem_layers
+
+        self.assertEqual(len(layers), 3)
+
+        self.assertTrue(set([x.id for x in layers[0]]) == set(["Base table"]))
+        self.assertTrue(set([x.id for x in layers[1]]) == set(["filter_column"]))
+        self.assertTrue(set([x.id for x in layers[2]]) == set(["Filtered table"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_table_product.py
@@ -0,0 +1,39 @@
+import unittest
+
+from prosto.Schema import *
+
+class TableProductTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_product(self):
+        sch = Schema("My schema")
+
+        t1 = sch.create_populate_table(
+            table_name="Table 1", attributes=["A"],
+            func="lambda **m: pd.DataFrame({'A': [1.0, 2.0, 3.0]})", tables=[], model=None, input_length='table'
+        )
+
+        t2 = sch.create_populate_table(
+            table_name="Table 2", attributes=["B"],
+            func="lambda **m: pd.DataFrame({'B': ['x', 'y', 'z']})", tables=[], model=None, input_length='table'
+        )
+
+        product = sch.create_product_table(
+            table_name="Product", attributes=["t1", "t2"],
+            tables=["Table 1", "Table 2"]
+        )
+
+        t1.evaluate()
+        t2.evaluate()
+        product.evaluate()
+
+        self.assertEqual(len(product.data.columns), 2)
+        self.assertEqual(len(product.data), 9)
+
+        self.assertEqual(product.data.columns.to_list(), ["t1", "t2"])
+
+
+if __name__ == '__main__':
+    unittest.main()
