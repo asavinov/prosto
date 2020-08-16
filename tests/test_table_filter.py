8382a738fd4c4cafe9ccc850dbe58e4c03891731
Alexandr Savinov <savinov@conceptoriented.org>
2020-08-16 13:26:51 +0200

    test for filter table augmentation via base column usage

--- tests/test_table_filter.py
+++ tests/test_table_filter.py
@@ -15,6 +15,7 @@ class TableFilterTestCase(unittest.TestCase):
             func="lambda **m: pd.DataFrame({'A': [1.0, 2.0, 3.0], 'B': ['x', 'yy', 'zzz']})", tables=[]
         )
 
+        # This (boolean) column will be used for filtering
         clm = sch.compute(
             name="filter_column", table=tbl.id,
             func="lambda x, param: (x['A'] > param) & (x['B'].str.len() < 3)",  # Return a boolean Series
@@ -47,6 +48,46 @@ class TableFilterTestCase(unittest.TestCase):
         self.assertTrue(set([x.id for x in layers[0]]) == set(["Base table"]))
         self.assertTrue(set([x.id for x in layers[1]]) == set(["filter_column"]))
         self.assertTrue(set([x.id for x in layers[2]]) == set(["Filtered table"]))
+
+    def test_filter_inheritance(self):
+        """Test topology augmentation. Use columns from the parent table by automatically adding the merge operation to topology."""
+        sch = Prosto("My Prosto")
+
+        base_tbl = sch.populate(
+            table_name="Base table", attributes=["A", "B"],
+            func="lambda **m: pd.DataFrame({'A': [1.0, 2.0, 3.0], 'B': ['x', 'yy', 'zzz']})", tables=[]
+        )
+
+        # This (boolean) column will be used for filtering
+        clm = sch.compute(
+            name="filter_column", table=base_tbl.id,
+            func="lambda x, param: (x['A'] > param) & (x['B'].str.len() < 3)",  # Return a boolean Series
+            columns=["A", "B"], model={"param": 1.5}
+        )
+
+        f_tbl = sch.filter(
+            table_name="Filtered table", attributes=["super"],
+            func=None, tables=["Base table"], columns=["filter_column"]
+        )
+
+        # In this calculate column, we use a column of the filtered table which actually exists only in the base table
+        clm = sch.calculate(
+            name="My column", table=f_tbl.id,
+            func="lambda x: x + 1.0", columns=["A"], model=None
+        )
+
+        sch.run()
+
+        clm_data = f_tbl.get_column_data('My column')
+
+        self.assertAlmostEqual(len(clm_data), 1)
+        self.assertAlmostEqual(clm_data[0], 3.0)
+
+        # This column had to be added automatically by the augmentation procedure
+        # It is inherited from the base table and materialized via merge operation
+        # It stores original values of the inherited base column
+        clm_data = f_tbl.get_column_data('A')
+        self.assertAlmostEqual(clm_data[0], 2)
 
 
 if __name__ == '__main__':
